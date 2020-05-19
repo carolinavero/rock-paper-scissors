8de9aae1146d6ba6962c36c8b5089db4f9e2dd54
Carolina Vero <carolina@w42.com.br>
2020-05-18 21:20:22 -0300

    update old files

--- scripts.test.js
+++
@@ -1,8 +0,0 @@
-const random = require('./scripts');
-
-describe('Tests for Rock, Paper and Scissors', () => {
-    it('Generate a random value', () => {
-        console.log('test for random')
-        expect(random).toBeLessThan(3)
-    })  
-})
